d9930bd40c5c4c3d3227bd9cbd8192bede59318e
Daniil Dzina <daniildzina@gmail.com>
2024-03-19 00:55:33 +0300

    upload whole project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel Application</title>
-    <!-- Ссылка на CSS Bootstrap для стилей (можете использовать любой другой фреймворк или свои стили) -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
@@ -12,7 +11,6 @@
         @yield('content')
     </div>
 
-    <!-- Ссылка на JavaScript Bootstrap (опционально, если используете компоненты Bootstrap, требующие JS) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
